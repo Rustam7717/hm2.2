2315754d133d4a3d7d158d0560c768bb47d03e48
Rustam7717 <rustam85@mail.ru>
2021-09-11 21:51:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static String name = "Rustam";
+    public static int age = 35;
+
+    public static void main(String[] args) {
+        System.out.println("My name is " + name + " I am at " + age + " years old");
+
+    }
+}
